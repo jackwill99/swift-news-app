5566932495127f82f7fbdd5f0ae0834a87873d96
Jack Will <65641460+jackwill99@users.noreply.github.com>
2023-12-23 14:25:49 +0630

    refactoring the user controller and service

--- src/mobile/users/users.controller.ts
+++ src/mobile/users/users.controller.ts
@@ -1,42 +1,58 @@
-import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
-import { UsersService } from './users.service';
-import { CreateUserDto } from './dto/create-user.dto';
-import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
-import { Public } from '../../_utils/necessary/public.decorator';
-import { AccessControl } from '../../_utils/acl/acl.decorator';
-import { AccessLevel } from '../../_utils/acl/acl.enum';
-import { MongoIdDto } from '../../_utils/dtos/mongo.id.dto';
+import { Body, Controller, Delete, Get, Param, Post } from "@nestjs/common";
+import { ApiBearerAuth, ApiOperation, ApiTags } from "@nestjs/swagger";
+import { errorResponse, successResponse } from "src/_utils/necessary/response";
+import { AccessControl } from "../../_utils/acl/acl.decorator";
+import { AccessLevel } from "../../_utils/acl/acl.enum";
+import { MongoIdDto } from "../../_utils/dtos/mongo.id.dto";
+import { Public } from "../../_utils/necessary/public.decorator";
+import { CreateUserDto } from "./dto/create-user.dto";
+import { UsersService } from "./users.service";
 
-@ApiTags('Users')
+@ApiTags("Users")
 @AccessControl.metaData(AccessLevel.ADMIN)
-@ApiBearerAuth('Authorization')
+@ApiBearerAuth("Authorization")
 @Controller()
 export class UsersController {
-  constructor(private readonly usersService: UsersService) {
-  }
+  constructor(private readonly usersService: UsersService) {}
 
-  @ApiOperation({ summary: 'Create Mobile User' })
+  @ApiOperation({ summary: "Create Mobile User" })
   @Post()
   @Public(true)
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.usersService.create(createUserDto);
+  async create(@Body() createUserDto: CreateUserDto) {
+    const user = await this.usersService.create(createUserDto);
+
+    return successResponse("Create User successfully", user);
   }
 
-  @ApiOperation({ summary: 'Get all of the mobile users' })
+  @ApiOperation({ summary: "Get all of the mobile users" })
   @Get()
-  findAll() {
-    return this.usersService.findAll();
+  async findAll() {
+    const users = await this.usersService.findAll();
+
+    return successResponse("All of user list", users);
   }
 
-  @ApiOperation({ summary: 'Take a user information' })
-  @Get(':id')
-  findOne(@Param() params: MongoIdDto) {
-    return this.usersService.findOne(params.id);
+  @ApiOperation({ summary: "Take a user information" })
+  @Get(":id")
+  async findOne(@Param() params: MongoIdDto) {
+    const user = await this.usersService.findOne(params.id);
+
+    if (user) {
+      return successResponse("User detail", user);
+    }
+
+    return errorResponse("User not found");
   }
 
-  @ApiOperation({ summary: 'Delete user' })
-  @Delete(':id')
-  remove(@Param() params: MongoIdDto) {
-    return this.usersService.remove(params.id);
+  @ApiOperation({ summary: "Delete user" })
+  @Delete(":id")
+  async remove(@Param() params: MongoIdDto) {
+    const user = await this.usersService.remove(params.id);
+
+    if (user) {
+      return successResponse("Successfully removed user");
+    }
+
+    return errorResponse("Failed to delete user");
   }
 }
--- src/mobile/users/users.service.ts
+++ src/mobile/users/users.service.ts
@@ -2,10 +2,6 @@ import { Injectable } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
 import { Model } from "mongoose";
 import { generateToken } from "../../_utils/necessary/access.token";
-import {
-  errorResponse,
-  successResponse,
-} from "../../_utils/necessary/response";
 import DBConnection from "../../constants/db";
 import { CreateUserDto } from "./dto/create-user.dto";
 import { User } from "./entities/user.entity";
@@ -16,35 +12,32 @@ export class UsersService {
     @InjectModel(User.name, DBConnection.coreDb) private userModel: Model<User>,
   ) {}
 
-  async create(createUserDto: CreateUserDto) {
+  async create(createUserDto: CreateUserDto): Promise<User> {
     const user = await this.userModel.create(createUserDto);
     user.token = generateToken({ role: createUserDto.role, id: user.id });
     await user.save();
 
-    return successResponse("Create User successfully", user);
+    return user;
   }
 
-  async findAll() {
-    const users = await this.userModel.find({ status: 1, delete: 0 });
-    return successResponse("All of user list", users);
+  async findAll(): Promise<User[]> {
+    const users = await this.userModel.find<User>({ status: 1, delete: 0 });
+    return users;
   }
 
-  async findOne(id: string) {
+  async findOne(id: string): Promise<User | null> {
     const user = await this.userModel.findById<User>(id, null, {
       status: 1,
       delete: 0,
     });
-    try {
-      if (user!) {
-        return successResponse("User Details", user);
-      }
-    } catch (e) {
-      return errorResponse(e);
-    }
+
+    return user;
   }
 
-  async remove(id: string) {
-    await this.userModel.findByIdAndUpdate(id, { status: 0 });
-    return successResponse("Successfully removed user");
+  async remove(id: string): Promise<User | null> {
+    const user = await this.userModel.findByIdAndUpdate<User>(id, {
+      status: 0,
+    });
+    return user;
   }
 }
