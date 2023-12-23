44601282216e8c1e3e55b495fd68d6ff2ae3dc3f
Jack Will <65641460+jackwill99@users.noreply.github.com>
2023-12-23 13:47:35 +0630

    bug fixed in resource api

--- src/mobile/resources/resources.service.ts
+++ src/mobile/resources/resources.service.ts
@@ -18,62 +18,70 @@ export class ResourcesService {
     private readonly countryService: CountryService,
   ) {}
 
+  private capitalize(str: string) {
+    return str
+      .split(" ")
+      .map((e) => e[0].toUpperCase() + e.substring(1))
+      .join(" ");
+  }
+
   async takeAll() {
     const apiKey = process.env["NEWS_DATA_IO"];
     // for (let i = 0; i < 31; i++) {
     //
     // }
     console.log(apiKey);
-    const response = await fetch(`${DBConnection.newsDataIo}${apiKey}`);
+    await fetch(`${DBConnection.newsDataIo}${apiKey}`).then(
+      async (response) => {
+        const data = await response.json();
+        const results = data["results"] as any[];
 
-    const data = await response.json();
-    const results = data["results"] as any[];
+        console.log(results);
+        for (const result of results) {
+          const countryName = this.capitalize(result["country"][0]);
 
-    console.log(results);
-    for (const result of results) {
-      const countryName = result["country"][0];
+          let resultCountry: Country;
+          const findCountry = await this.countryService.findByName(countryName);
+          if (!findCountry) {
+            const country = new CreateCountryDto();
+            country.name = countryName;
+            const countryResponse = await this.countryService.create(country);
+            resultCountry = countryResponse;
+          } else {
+            resultCountry = findCountry;
+          }
 
-      let resultCountry: Country;
-      const findCountry = await this.countryService.findByName(countryName);
-      if (!findCountry) {
-        const country = new CreateCountryDto();
-        const countryUpperCase = (result["country"][0] as string)
-          .split(" ")
-          .map((e) => e[0].toUpperCase() + e.substring(1))
-          .join(" ");
-        country.name = countryUpperCase;
-        const countryResponse = await this.countryService.create(country);
-        resultCountry = countryResponse;
-      } else {
-        resultCountry = findCountry;
-      }
+          const resultCategories: Category[] = [];
+          for (const i of result["category"] as string[]) {
+            const categoryName = this.capitalize(i);
+            const findCategory =
+              await this.categoryService.findByName(categoryName);
 
-      const resultCategories: Category[] = [];
-      for (const i of result["category"] as string[]) {
-        const findCategory = await this.categoryService.findByName(i);
+            if (!findCategory) {
+              const category = new CreateCategoryDto();
+              category.name = categoryName;
+              const categoryResponse =
+                await this.categoryService.create(category);
+              resultCategories.push(categoryResponse);
+            } else {
+              resultCategories.push(findCategory);
+            }
+          }
 
-        if (!findCategory) {
-          const category = new CreateCategoryDto();
-          category.name = i;
-          const categoryResponse = await this.categoryService.create(category);
-          resultCategories.push(categoryResponse);
-        } else {
-          resultCategories.push(findCategory);
+          const news = new CreateNewsDto();
+          news.title = result["title"];
+          news.creator = result["creator"];
+          news.link = result["link"];
+          news.description = result["description"];
+          news.content = result["content"];
+          news.imgUrl = result["imgUrl"];
+          news.pubDate = result["pubDate"];
+          news.categories = resultCategories;
+          news.country = resultCountry;
+          await this.newsService.create(news);
         }
-      }
-
-      const news = new CreateNewsDto();
-      news.title = result["title"];
-      news.creator = result["creator"];
-      news.link = result["link"];
-      news.description = result["description"];
-      news.content = result["content"];
-      news.imgUrl = result["imgUrl"];
-      news.pubDate = result["pubDate"];
-      news.categories = resultCategories;
-      news.country = resultCountry;
-      await this.newsService.create(news);
-    }
+      },
+    );
 
     return `Data are fetching and inserting into database. Don't call within 5 minutes for the next times.`;
   }
