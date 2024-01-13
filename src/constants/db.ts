c0914ac36ebeb9ccfb29b75cd0d246555ba78fa8
Jack Will <65641460+jackwill99@users.noreply.github.com>
2024-01-13 17:50:30 +0630

    bug fixed

--- src/constants/db.ts
+++ src/constants/db.ts
@@ -3,7 +3,7 @@ export default class DBConnection {
   static readonly newsDataIo = "https://newsdata.io/api/1/news?apikey=";
   // static readonly host_1 = "http://192.168.100.24:5001";
 
-  static readonly server_1 = "https://swiftness.cyclic.app";
+  static readonly server_1 = "https://swiftnews.cyclic.app";
 
   static readonly coreDb = "core";
 }
